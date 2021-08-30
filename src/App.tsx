bdeb0936ddbd6be3ff773486c31f600b548d11f0
Mitzi <mitzisantos13@gmail.com>
2021-08-30 12:53:07 -0300

    Stop form from reloading the page on submit

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
+import { FormEvent } from "react";
 import { TodoList } from "components/TodoList";
 
-export const App = () => (
-  <div>
-    <h1>Todo</h1>
-    <form>
-      <input
-        type="text"
-        aria-label="Write a new todo item"
-        placeholder="Create a new todo..."
-      />
-    </form>
-    <TodoList todos={[]} />
-  </div>
-);
+export const App = () => {
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div>
+      <h1>Todo</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          aria-label="Write a new todo item"
+          placeholder="Create a new todo..."
+        />
+      </form>
+      <TodoList todos={[]} />
+    </div>
+  );
+};
