b3c6a89555e9d394818b91c9be340c2f272d9518
Mitzi <mitzisantos13@gmail.com>
2021-08-29 21:06:04 -0300

    Add a form for adding todos

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,13 @@ import { TodoList } from "components/TodoList";
 export const App = () => (
   <div>
     <h1>Todo</h1>
+    <form>
+      <input
+        type="text"
+        aria-label="Write a new todo item"
+        placeholder="Create a new todo..."
+      />
+    </form>
     <TodoList todos={[]} />
   </div>
 );
