a5061e8b6ce3304a862364ed95585ee4574ce3e1
Mitzi <mitzisantos13@gmail.com>
2021-08-31 16:03:52 -0300

    Update TodoList to receive children

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,15 +1,16 @@
 import { TodoItem } from "components/TodoItem";
-import { forwardRef } from "react";
+import { forwardRef, ReactNode } from "react";
 import { Todo } from "types/Todo";
 
 type TodoListProps = {
+  children?: ReactNode;
   todos: Todo[];
   toggleTodo: (index: number) => void;
   deleteTodo: (index: number) => void;
 };
 
 export const TodoList = forwardRef<HTMLUListElement, TodoListProps>(
-  ({ todos, toggleTodo, deleteTodo }: TodoListProps, ref) => (
+  ({ children, todos, toggleTodo, deleteTodo }: TodoListProps, ref) => (
     <ul ref={ref}>
       {todos.map((todo, index) => (
         <TodoItem
@@ -20,6 +21,7 @@ export const TodoList = forwardRef<HTMLUListElement, TodoListProps>(
           deleteTodo={deleteTodo}
         />
       ))}
+      {children}
     </ul>
   )
 );
