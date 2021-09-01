5f4d287b560b74bf91def3bb5f5e7deed2dbfdb8
Mitzi <mitzisantos13@gmail.com>
2021-09-01 17:51:39 -0300

    Add a decorative checkbox to the side of the new todo text input

--- src/components/NewTodoTextInput.tsx
+++ src/components/NewTodoTextInput.tsx
@@ -1,21 +1,45 @@
 import { FormEvent } from "react";
+import styled from "styled-components";
 import { Input } from "components/Input";
 
+export { StyledNewTodoTextInput as NewTodoTextInput };
+
 type NewTodoTextInputProps = {
+  className?: string;
   newTodoText: string;
   onChange: (e: FormEvent<HTMLInputElement>) => void;
 };
 
-export const NewTodoTextInput = ({
+const NewTodoTextInput = ({
+  className,
   newTodoText,
   onChange,
 }: NewTodoTextInputProps) => (
-  <Input
-    type="text"
-    name="addTodo"
-    aria-label="Write a new todo item"
-    value={newTodoText}
-    onChange={onChange}
-    placeholder="Create a new todo..."
-  />
+  <div className={className}>
+    <Checkbox />
+    <Input
+      type="text"
+      name="addTodo"
+      aria-label="Write a new todo item"
+      value={newTodoText}
+      onChange={onChange}
+      placeholder="Create a new todo..."
+    />
+  </div>
 );
+
+const StyledNewTodoTextInput = styled(NewTodoTextInput)`
+  display: flex;
+  align-items: center;
+  background: #fff;
+  border-radius: 5px;
+`;
+
+const Checkbox = styled.div`
+  flex-shrink: 0;
+  margin-left: 1.25rem;
+  width: 1.25rem;
+  height: 1.25rem;
+  border-radius: 50%;
+  border: 1px solid #e4e5f1;
+`;
