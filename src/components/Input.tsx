aac7ccead8852946d9f1e5fb7e6ed9b090cb6aac
Mitzi <mitzisantos13@gmail.com>
2021-09-01 15:43:57 -0300

    Set color of the placeholder of Input

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,7 +1,26 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
+
+const placeholderStyles = css`
+  color: #9394a5;
+`;
 
 export const Input = styled.input`
   width: 100%;
   border: none;
   border-radius: 5px;
+
+  ::-webkit-input-placeholder {
+    ${placeholderStyles}
+  }
+
+  ::-moz-placeholder {
+    ${placeholderStyles}
+  }
+
+  :-ms-input-placeholder {
+    ${placeholderStyles}
+  }
+  :-moz-placeholder {
+    ${placeholderStyles}
+  }
 `;
