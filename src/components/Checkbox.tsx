1b554116faf3c29b76c6d267cdef3b7493330e21
Mitzi <mitzisantos13@gmail.com>
2021-09-05 13:07:01 -0300

    Refactor Checkbox to use locally defined components

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -13,10 +13,10 @@ type CheckboxProps = {
 
 const Checkbox = ({ className, id, isChecked, onChange }: CheckboxProps) => (
   <div className={className}>
-    <input id={id} type="checkbox" onChange={onChange} checked={isChecked} />
-    <div className="control">
-      <CheckIcon />
-    </div>
+    <Input id={id} type="checkbox" onChange={onChange} checked={isChecked} />
+    <Control>
+      <StyledCheckIcon />
+    </Control>
   </div>
 );
 
@@ -31,62 +31,62 @@ const StyledCheckbox = styled(Checkbox)`
   > * {
     grid-area: checkbox;
   }
+`;
 
-  input {
+const Input = styled.input`
+  opacity: 0;
+  margin: 0;
+  width: 100%;
+  height: 100%;
+`;
+
+const Control = styled.div`
+  border-radius: 50%;
+  border: 1px solid #e4e5f1;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  transition-property: border;
+  transition-duration: 0.3s;
+
+  &::before {
+    transform: scale(0);
     opacity: 0;
-    margin: 0;
+    pointer-events: none;
+    content: "";
+    display: block;
     width: 100%;
     height: 100%;
-  }
-
-  .control {
     border-radius: 50%;
-    border: 1px solid #e4e5f1;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    transition-property: border;
+    position: absolute;
+    background: linear-gradient(
+      to left top,
+      hsl(280, 87%, 65%),
+      hsl(192, 100%, 67%)
+    );
     transition-duration: 0.3s;
-
-    svg {
-      transform: scale(0);
-      opacity: 0;
-      pointer-events: none;
-      z-index: 1;
-      transition-duration: 0.3s;
-      transition-property: opacity, transform;
-    }
-
-    ::before {
-      transform: scale(0);
-      opacity: 0;
-      pointer-events: none;
-      content: "";
-      display: block;
-      width: 100%;
-      height: 100%;
-      border-radius: 50%;
-      position: absolute;
-      background: linear-gradient(
-        to left top,
-        hsl(280, 87%, 65%),
-        hsl(192, 100%, 67%)
-      );
-      transition-duration: 0.3s;
-      transition-property: opacity, transform;
-    }
+    transition-property: opacity, transform;
   }
 
-  input:checked + .control {
+  ${Input}:checked + & {
     border: 0;
-
-    svg {
-      opacity: 1;
-      transform: scale(1);
-    }
   }
 
-  input:checked + .control::before {
+  ${Input}:checked + &::before {
+    opacity: 1;
+    transform: scale(1);
+  }
+`;
+
+const StyledCheckIcon = styled(CheckIcon)`
+  transform: scale(0);
+  opacity: 0;
+  pointer-events: none;
+  z-index: 1;
+  transition-duration: 0.3s;
+  transition-property: opacity, transform;
+
+  ${Input}:checked + ${Control} & {
     opacity: 1;
     transform: scale(1);
   }
